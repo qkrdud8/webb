74ce508ae01f09b38bbb5dafb6b09ce94d4af80d
ByeongGiKim <ekdhqudrl12@gmail.com>
2022-06-13 01:07:56 +0900

    silde add

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 document.querySelector('.left_arrow_1').addEventListener('click', function(){
 
-    document.querySelector('.container_2').style.display = 
-            'none'
     document.querySelector('.container_1').style.display = 
-            'block'
+                'block'
+    document.querySelector('.container_2').style.display = 
+                'none'
 })
 
 document.querySelector('.right_arrow_1').addEventListener('click', function(){
     
-    document.querySelector('.container_1').style.display = 
-            'none'
-    document.querySelector('.container_2').style.display = 
-            'block'
-
+        document.querySelector('.container_1').style.display = 
+                'none'
+        document.querySelector('.container_2').style.display = 
+                'block'
 })
 
+document.querySelector('.left_arrow_2').addEventListener('click', function(){
+
+        document.querySelector('.container_3').style.display = 
+                    'block'
+        document.querySelector('.container_4').style.display = 
+                    'none'
+    })
+    
+    document.querySelector('.right_arrow_2').addEventListener('click', function(){
+        
+            document.querySelector('.container_3').style.display = 
+                    'none'
+            document.querySelector('.container_4').style.display = 
+                    'block'
+    })
+    
